0a35c9359dbebd0d728306af75a74517c86e147a
Your Name <you@example.com>
2017-04-22 06:04:16 -0500

    changed util require path in sibilant context

--- editor.el
+++ editor.el
@@ -1,6 +1,13 @@
-(defun )
 (async-shell-command "sibilant" "*Sibilant*")
+
 (process-send-string "*Sibilant*" "(console.log \"hi\")")
 (process-send-string "*Sibilant*" "\n")
+
 (buffer-string)
-(asso "header.sibilant")
+;; (defvar basename file-name-nondirectory)
+;; (defvar dirname file-name-directory)
+(defun open-named-eshell (name)
+  (eshell)
+  (rename-buffer name ))
+
+(open-named-eshell "foobar")
