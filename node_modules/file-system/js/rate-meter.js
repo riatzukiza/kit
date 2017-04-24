31f7aeb8a351efa58de56dcc8efa25db40f56f2a
Your Name <you@example.com>
2017-04-24 06:23:27 -0500

    update triggered from down stream repl repo

--- node_modules/file-system/js/rate-meter.js
+++ node_modules/file-system/js/rate-meter.js
@@ -1,10 +1,10 @@
 type(String);
 describe(String, init(path()));
 type(Timer);
-describe(RateMeter, Date.now().slice(-1)[0], init(emitter(event, callback(this.update.bind(this)))), gmth(start, emitter(event, timer(this), callback(->(timer.update()))), , timer._running = true;
+describe(RateMeter, Date.now().slice(-1)[0], init(emitter(event, callback(this.update.bind(this)))), gmth(start, emitter(event, timer(this), callback(->(timer.update()))), timer._running = true;
 timer.callback = callback;, emitter.on(event)), gmth(stop, emitter(event, callback), timer._running = false;, emitter.removeListeners("event", callback)), gmth(restart, null), gmth(update, null, timer.last = Date.now();));
 var defer = (function defer$(f, args) {
-  /* defer ../file-system/sib/rate-meter.sibilant:27:0 */
+  /* defer ../file-system/sib/rate-meter.sibilant:25:0 */
 
   return =>(args());
 });
