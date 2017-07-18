bb028dd1a1afd94136468dab83a90969afb498cd
Your Name <you@example.com>
2017-07-17 19:21:18 -0500

    added git ignore

--- node_modules/file-system/install.sh
+++
@@ -1,2 +0,0 @@
-rm -r node_modules/;
-npm install;
--- node_modules/file-system/js/fs-node.js
+++
@@ -1,2 +0,0 @@
-specify(FSNode, extend(EventEmitter.prototype));
-defDescription(FSNode, path(fs), init(EventEmitter.call(this)), gett(stats, stat(this.path)), defGeneric(watch, path(fs), fs.watch(path)));
